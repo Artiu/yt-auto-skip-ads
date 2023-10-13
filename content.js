d57e660220c1710a2b486182f9a9b7936526918e
Artiu <oborskiartur123@gmail.com>
2023-10-13 18:12:34 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,24 @@
+let skipOnCooldown = false;
+
+const setSkipOnCooldown = () => {
+	skipOnCooldown = true;
+	setTimeout(() => {
+		skipOnCooldown = false;
+	}, 200);
+};
+
+const observer = new MutationObserver(() => {
+	const isAdPlaying = document.querySelector("div.ad-showing");
+	if (!isAdPlaying) return;
+	const [skipButton] = document.getElementsByClassName("ytp-ad-skip-button");
+	if (skipButton) {
+		setSkipOnCooldown();
+		skipButton.click();
+		return;
+	}
+	const [videoElement] = document.getElementsByClassName("video-stream html5-main-video");
+	if (!videoElement.duration || skipOnCooldown) return;
+	setSkipOnCooldown();
+	videoElement.currentTime = videoElement.duration;
+});
+observer.observe(document.body, { childList: true, subtree: true });
