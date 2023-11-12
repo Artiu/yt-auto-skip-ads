d5df0dd1c9ded4e443254e7b379593e425ab922d
Artiu <oborskiartur123@gmail.com>
2023-11-12 15:05:45 +0100

    Add handle for new skip button

--- content.js
+++ content.js
@@ -16,6 +16,12 @@ const observer = new MutationObserver(() => {
 		skipButton.click();
 		return;
 	}
+	const [modernSkipButton] = document.getElementsByClassName("ytp-ad-skip-button-modern");
+	if (modernSkipButton) {
+		setSkipOnCooldown();
+		modernSkipButton.click();
+		return;
+	}
 	const [videoElement] = document.getElementsByClassName("video-stream html5-main-video");
 	if (!videoElement.duration || skipOnCooldown) return;
 	setSkipOnCooldown();
